a49910db558d40b5c76e0a817902d2c246afefba
Kelly <kellyandstone@gmail.com>
2019-03-14 23:25:55 +1300

    touch layout.pug really similar handlebar

--- nodejs-blog/app.js
+++ nodejs-blog/app.js
@@ -7,7 +7,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", function(req, res) {
-  res.render("index");
+  res.render("index", {
+    title: "title is Node js blog"
+  });
+});
+
+app.get("/articles/new", function(req, res) {
+  res.render("new", {
+    title: "Add Article"
+  });
 });
 
 app.listen(3000, function() {
