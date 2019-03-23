73b1f05f89690e7f2b85a97d8e4b2e56125bdbd3
Kelly <kellyandstone@gmail.com>
2019-03-23 22:49:47 +1300

    first time learning jquery, to bring the console.log showing same id as browser shows

--- nodejs-blog/public/js/main.js
+++ nodejs-blog/public/js/main.js
@@ -1,1 +1,7 @@
-console.log("hello");
+$(document).ready(function() {
+  $(".delete-article").on("click", function(e) {
+    //className .delete-article from show.pug
+    $target = $(e.target);
+    console.log($target.attr("data-id"));
+  });
+});
