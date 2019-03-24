7b3accd81ae63299589f075384419978b0dcb4a7
Kelly <kellyandstone@gmail.com>
2019-03-24 21:23:10 +1300

    mr-2, means space

--- nodejs-blog/public/js/main.js
+++ nodejs-blog/public/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   $(".delete-article").on("click", function(e) {
     //className .delete-article from show.pug
-    $target = $(e.target);
-    console.log($target.attr("data-id"));
+    var $target = $(e.target);
+    //console.log($target.attr("data-id"));
+    var id = $target.attr("data-id");
   });
 });
