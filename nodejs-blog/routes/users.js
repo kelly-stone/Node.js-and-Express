788ffe7a56e1a5e9117e1d5d98f4c98786a2cd7c
Kelly <kellyandstone@gmail.com>
2019-04-13 23:10:54 +1200

    reading from https://github.com/jaredhanson/passport

--- nodejs-blog/routes/users.js
+++ nodejs-blog/routes/users.js
@@ -74,6 +74,24 @@ router.get("/login", function(req, res) {
   res.render("users/login");
 });
 
-router.post("/login", function(req, res) {});
-
+router.post("/login", function(req, res) {
+  //https://github.com/jaredhanson/passport
+  //http://www.passportjs.org/docs/authenticate/
+});
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (!user.verifyPassword(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
 module.exports = router;
