a3dcbcaf707af28659f837148c17a06185fa7078
Kelly <kellyandstone@gmail.com>
2019-04-14 22:03:34 +1200

    create config/passport.js

--- nodejs-blog/routes/users.js
+++ nodejs-blog/routes/users.js
@@ -78,20 +78,20 @@ router.post("/login", function(req, res) {
   //https://github.com/jaredhanson/passport
   //http://www.passportjs.org/docs/authenticate/
 });
-passport.use(
-  new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false);
-      }
-      if (!user.verifyPassword(password)) {
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-  })
-);
+// passport.use(
+//   new LocalStrategy(function(username, password, done) {
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) {
+//         return done(err);
+//       }
+//       if (!user) {
+//         return done(null, false);
+//       }
+//       if (!user.verifyPassword(password)) {
+//         return done(null, false);
+//       }
+//       return done(null, user);
+//     });
+//   })
+// );
 module.exports = router;
