0838c9cc60131839ba468f63fd69626b2c33aaaf
Kelly <kellyandstone@gmail.com>
2019-04-15 12:11:01 +1200

    http://www.passportjs.org/docs/configure/

--- nodejs-blog/config/passport.js
+++ nodejs-blog/config/passport.js
@@ -1,16 +1,20 @@
-passport.use(
-  new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false);
-      }
-      if (!user.verifyPassword(password)) {
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-  })
-);
+const LocalStrategy = require("passport-local").Strategy; //http://www.passportjs.org/docs/configure/
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy(function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!user.verifyPassword(password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+};
