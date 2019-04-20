afbd7a6ca4177d70549eb6466641bba15c528e43
Kelly <kellyandstone@gmail.com>
2019-04-20 19:27:43 +1200

    create a logou btn when there is a user

--- nodejs-blog/app.js
+++ nodejs-blog/app.js
@@ -43,6 +43,11 @@ require("./config/passport")(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get("*", function(req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
 let Article = require("./models/article");
 
 app.set("views", path.join(__dirname, "views"));
